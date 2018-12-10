c30b34b1713f341401fbd9d5876b4db8c57d9440
ghicheon <gebmania@gmail.com>
2018-12-10 09:18:17 +0900

    Update workspace_udacity__.sh

--- workspace_udacity__.sh
+++ workspace_udacity__.sh
@@ -1,3 +1,5 @@
+#CarND-Capstone_ : mine
+#CarND-Capstone : initial version
 cp CarND-Capstone_/ros/src/waypoint_updater/waypoint_updater.py           CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py 
 cp CarND-Capstone_/ros/src/tl_detector/tl_detector.py                     CarND-Capstone/ros/src/tl_detector/tl_detector.py 
 cp CarND-Capstone_/ros/src/twist_controller/twist_controller.py           CarND-Capstone/ros/src/twist_controller/twist_controller.py 
